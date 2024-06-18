d3da20536252e33a5545676a1e7cb5573c71a606
bigfatjones <sidmcmillan@gmail.com>
2024-06-18 16:14:04 -0700

    Update dropmouse.py

--- dropmouse.py
+++ dropmouse.py
@@ -1,4 +1,3 @@
-# dropmouse.py
 from flask import Flask, request, render_template
 import requests
 from bs4 import BeautifulSoup
@@ -24,7 +23,7 @@ def send_email(sender_email, receiver_email, password, subject, message):
 
     server = smtplib.SMTP('smtp.gmail.com', 587)
     server.starttls()
-    server.login(sender_email, password.encode('utf-8'))  # Encode the password using UTF-8
+    server.login(sender_email, password)  # No need to encode the password again
     server.sendmail(sender_email, receiver_email, msg.as_string())
     server.quit()
 
